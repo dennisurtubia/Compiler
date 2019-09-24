b1a8f9691a0d24b1a7251e26c592d3762c26c80a
dennisurtubia <dennisurtubia@gmail.com>
2019-09-24 15:12:04 -0300

    Inicia analisador sintatico

---
+++ Syntatic/implementacao/syntatic.py
@@ -0,0 +1,32 @@
+import ply.yacc as yacc
+
+def p_programa():
+    lista_declaracoes
+
+def p_lista_declaracoes():
+    pass
+
+def p_declaracao():
+    pass
+
+def p_declaracao_variaveis():
+    pass
+
+def p_inicializacao_variaveis():
+    pass
+
+def p_declaracao_funcao():
+    pass
+
+def p_lista_variaveis():
+    pass
+
+def p_cabecalho():
+    pass
+
+def p_listas_parametros():
+    pass
+
+def p_acao():
+    pass
+
